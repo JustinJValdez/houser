bc08cc36b75b7d60d8f8110e410d4ce88d7e0738
Justin Valdez <justinjvaldez@gmail.com>
2018-09-19 19:45:46 -0600

    fourth

---
+++ server/index.js
@@ -0,0 +1,35 @@
+const express = require('express'),
+    cors = require('cors'),
+    bodyparser = require('body-parser'),
+   // massive = require('massive'),
+    axios = require('axios'),
+    controller = require('./controller')
+require('dotenv').config();
+
+
+
+const app = express();
+
+
+/*massive(process.env.CONNECTION_STRING)
+    .then(dbinstance => {
+    app.set('db', dbinstance)
+    console.log(`DB is Running`)
+    }).catch( err=>console.log(err));*/
+
+
+app.use(cors());
+app.use(bodyparser.json());
+
+//endpoint=''
+//CB function that acts as DB, db.PR.find = .then() gets inven.. from products_table and res.send it back to front end
+/*app.get(`/api/inventory`, controller.getProductsTable)
+
+app.post(`/api/product`, controller.postProductsTable)*/
+
+
+const port = process.env.SERVER_PORT || 9000
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
